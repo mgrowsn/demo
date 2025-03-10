8ffcdfad690c41b6c226d82407b2cf8a5ecfdef6
mehak_j <mehak_j@epam.com>
2025-03-10 11:01:09 +0530

    committed

--- task04/app/lambdas/sns_handler/index.js
+++ task04/app/lambdas/sns_handler/index.js
@@ -1,11 +1,9 @@
 exports.handler = async (event) => {
-    // Loop through each SNS record (message)
     event.Records.forEach(record => {
         const snsMessage = record.Sns.Message; // Access the SNS message content
         console.log("Message received from SNS:", snsMessage); // Log to CloudWatch Logs
     });
 
-    // Return a success response
     return {
         statusCode: 200,
         body: JSON.stringify({ message: "Messages processed successfully" })
--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,11 +1,9 @@
 exports.handler = async (event) => {
-    // Loop through each record received from SQS
     event.Records.forEach(record => {
         const messageBody = record.body; // Get the message body from the record
         console.log("Message received from SQS:", messageBody); // Log to CloudWatch Logs
     });
 
-    // Return a success response
     return {
         statusCode: 200,
         body: JSON.stringify({ message: "Messages processed successfully" })
